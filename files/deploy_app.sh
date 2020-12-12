524ee6f37921aa4073d4c9254b83449fe05d2e23
chrdal75 <chrdal@gmail.com>
2020-12-11 18:11:55 -0600

    Update deploy_app.sh

--- files/deploy_app.sh
+++ files/deploy_app.sh
@@ -4,14 +4,14 @@
 
 cat << EOM > /var/www/html/index.html
 <html>
-  <head><title>Meow!</title></head>
+  <head><title>Woof!</title></head>
   <body>
   <div style="width:800px;margin: 0 auto">
 
   <!-- BEGIN -->
   <center><img src="http://${PLACEHOLDER}/${WIDTH}/${HEIGHT}"></img></center>
-  <center><h2>Meow World!</h2></center>
-  Welcome to ${PREFIX}'s app. Everyone loves a cat !!!
+  <center><h2>Woof World!</h2></center>
+  Welcome to ${PREFIX}'s app. Everyone loves a DOG !!!
   <!-- END -->
 
   </div>
